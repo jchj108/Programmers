707af42e1b6b2d14e990fe2f689fabe42a5d4249
jchj108 <jchj108@naver.com>
2021-05-07 01:09:04 +0900

    프로그래머스 레벨 1

---
+++ src/level1/test/Squared.java
@@ -0,0 +1,28 @@
+package level1.test;
+
+import java.util.Scanner;
+
+public class Squared {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		long answer = 0;
+		
+		for(int i = 0; i < n; i++) {
+			
+			if(i*i==n) {
+				answer = (i+1)*(i+1);
+				break;
+			} else {
+				answer = -1;
+			}
+		}
+		System.out.println(answer);
+		
+		
+	}
+
+}

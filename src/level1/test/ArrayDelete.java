ade511cc40cdad5034bcc295ed60cdd10133bd62
jchj108 <jchj108@naver.com>
2021-05-12 03:19:56 +0900

    문자열 다루기, 배열 뒤집기

--- src/level1/test/ArrayDelete.java
+++ src/level1/test/ArrayDelete.java
@@ -1,18 +1,40 @@
 package level1.test;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-
-//import java.util.Collections;
 
 public class ArrayDelete {
 
-	public static void main(String[] args) {
-		
-		ArrayList<integer> list = new ArrayList<>();
-		
-		
-	}
-	
+	private static int[] solution(int[] arr) {
+		if (arr.length == 1) {
+			return new int[] { -1 };
+		}
 
+		int min = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			min = Math.min(min, arr[i]);
+		}
+		
+		int[] answer = new int[arr.length - 1];
+		int j = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == min) {
+				continue;
+			} 
+			answer[j++] = arr[i];
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+
+		int[] arr1 = { 1, 15, 2, 33, 5 };
+
+		int[] result = {};
+		result = ArrayDelete.solution(arr1);
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
 }
